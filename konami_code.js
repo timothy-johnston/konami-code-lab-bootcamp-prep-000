147d4aa37d82bb47e98b9272c74259ca96e950af
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 12:48:40 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -7,7 +7,7 @@ function init() {
   const body=document.body;
   
   body.addEventListener('keydown', function(e) {
-    const key = parseInt(e.deail || e.which);
+    const key = parseInt(e.detail || e.which);
     
     if (key === code [index]) {
       index++

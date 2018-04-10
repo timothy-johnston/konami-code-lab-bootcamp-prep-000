f0e1133596d324815f1461e5cd0b46aa15a35d20
tj11 Johnston <tedwardj11@gmail.com>
2018-04-10 12:04:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  //Keep track of index outside of the event handler.
+  let index = 0;
+  
+  //This is the function evoked by the event listener.
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+  }
 
 }
